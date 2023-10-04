333970b2ea8f2b19e535f35307a715ec4173cccd
PDeRivas <p.derivas@itecriocuarto.org.ar>
2023-10-04 15:32:12 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Proyecto integrador de la materia Javascript utilizando Reactjs
+
+Se crearon 2 componentes:
+
+-Un componente DrawClass que nos permita dibujar una clase UML.
+    Le pasaremos como parametro el nombre de la clase, sus atributos y metodos, ademas del borde de la clase y el fondo del titulo
+
+-Un componente Menu.
+    Este nos mostrara una barra con componentes en horizontal, al pasarle el mouse a alguno mostrara debajo de el todos los elementos que se desplieguen de el. En el submenu apareceran elementos y si esto tienen submenues deberan mostrarse a la derecha.
+
+    Pasaremos como argumentos para el menu una configuracion de los colores, un id para el menu superior y los atributos de cada componente submenu.
---
+++ src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import React from 'react';
+import MenuBar from './components/menu';
+
+function App() {
+  const configMenu = {
+    configColor: {
+      background: '#f4f5fa', // Color de Fondo General de la botonera
+      itemBackground: '#d0d0d0',// Color de Fondo de los subMenús
+      itemColor: '#666', // Color del texto de cada item del menú
+      itemActive: '#a8a8a8', // Color cuando hace click y se abre un submenú
+    },
+
+    id: 150,
+
+    menuItems: [
+      { name: 'Another Action', isFolder: false, id: 148, idPadre: 150 },
+      { name: 'sub menu', isFolder: true, id: 2, idPadre: 150 },
+      { name: 'Action', isFolder: false, id: 3, idPadre: 2 },
+
+      { name: 'Another action', isFolder: false, id: 4, idPadre: 2 },
+      { name: 'sub menu', isFolder: true, id: 5, idPadre: 2 },
+      { name: 'Another action', isFolder: false, id: 55, idPadre: 5 },
+      { name: 'something else here', isFolder: false, id: 56, idPadre: 5 },
+    ],
+  } // end
+
+
+  return (
+    <div className="App">
+      <MenuBar config={configMenu}></MenuBar>
+    </div>
+  )
+}
+
+export default App;
