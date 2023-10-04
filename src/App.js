1f686e52037503e2ae8620ad79e698c92c6cab32
simonlombardi <s.lombardi@itecriocuarto.org.ar>
2023-10-04 17:30:48 -0300

    drawclass

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import DrawClass from './components/DrawClass';
 
 function App() {
+
+  const CLASS_UML = {
+    name: 'Person',
+    attributes: ['+name:str','+phoneNumber:str','+emailAddress:str'],
+    methods:['+purcharseParkinPass()'],
+    borderColor:'#DC2DDE',
+    headColor:'#DC2DDE',
+    textColor:'#333333'
+    }
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DrawClass data={CLASS_UML} />
     </div>
   );
 }
---
+++ src/components/DrawClass.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+
+export default function DrawClass( {data} ){
+    const {name, attributes, methods, borderColor, headColor, textColor} = data
+
+    function getEntireList(){
+        let listAttributesAndMethods = []
+        try{
+            for(let i = 0; i < attributes.length; i++){
+            listAttributesAndMethods.push(attributes[i])
+    }
+        for(let i = 0; i < methods.length; i++){
+            listAttributesAndMethods.push(methods[i])
+        }
+    return listAttributesAndMethods
+        }
+        catch{
+            console.log("error");
+        }
+        
+    }
+    
+    function calcMaxWidth(){
+        let elementMaxWidth = listAttributesAndMethods[0] //suponemos que el primer elemento de la lista es el que tiene mas caracteres
+        try{
+           for (let i = 1; i < listAttributesAndMethods.length; i++) {
+            if (listAttributesAndMethods[i].length > elementMaxWidth.length) {
+                elementMaxWidth = listAttributesAndMethods[i]; 
+            }
+        }
+        return elementMaxWidth.length 
+        }
+        catch{
+            return 20
+        }
+        
+    }
+
+    const listAttributesAndMethods = getEntireList()
+    const maxWidth = calcMaxWidth()*10
+    let attributeHeigth
+    if (attributes.length !== 0){
+        attributeHeigth = attributes.length*40
+    }
+    else {
+        attributeHeigth = 80
+    }
+    let methodHeigth
+    if (methods.length !== 0){
+        methodHeigth = methods.length*35 + attributeHeigth
+    }
+    else {
+        methodHeigth = 35 + attributeHeigth
+    }
+    
+
+    return(
+    
+        <svg width={maxWidth} height={methodHeigth} >
+            <rect width={maxWidth} height="35" fill={headColor} strokeWidth="2" stroke={borderColor}/>
+            <text x="20" y="23" fill={textColor}>{name}</text>
+            <rect width={maxWidth} height={methodHeigth} fill="transparent" strokeWidth="2" stroke={borderColor}/>
+            {attributes.map((attributes, index) => <text x="10" y={index*20 + 60} fill={textColor} key={index}>{attributes}</text>)}
+            <line x1="0" y1={attributeHeigth} x2={maxWidth} y2={attributeHeigth} stroke={borderColor} strokeWidth="1.5"/>
+            {methods.map((methods, index) => <text x="10" y={attributeHeigth + (index+1)*20} fill={textColor} key={index}>{methods}</text>)}
+        </svg>
+    )
+}
