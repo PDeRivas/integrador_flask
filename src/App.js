bb0ac0e8bb53326917445093122019c4cdd59c55
PDeRivas <p.derivas@itecriocuarto.org.ar>
2023-10-05 17:30:31 -0300

    Primeros cambios

--- src/App.js
+++ src/App.js
@@ -1,36 +1,23 @@
 import './App.css';
 import React from 'react';
-import MenuBar from './components/menu';
+import DrawClass from './components/DrawClass';
 
 function App() {
-  const configMenu = {
-    configColor: {
-      background: '#f4f5fa', // Color de Fondo General de la botonera
-      itemBackground: '#d0d0d0',// Color de Fondo de los subMenús
-      itemColor: '#666', // Color del texto de cada item del menú
-      itemActive: '#a8a8a8', // Color cuando hace click y se abre un submenú
-    },
 
-    id: 150,
-
-    menuItems: [
-      { name: 'Another Action', isFolder: false, id: 148, idPadre: 150 },
-      { name: 'sub menu', isFolder: true, id: 2, idPadre: 150 },
-      { name: 'Action', isFolder: false, id: 3, idPadre: 2 },
-
-      { name: 'Another action', isFolder: false, id: 4, idPadre: 2 },
-      { name: 'sub menu', isFolder: true, id: 5, idPadre: 2 },
-      { name: 'Another action', isFolder: false, id: 55, idPadre: 5 },
-      { name: 'something else here', isFolder: false, id: 56, idPadre: 5 },
-    ],
-  } // end
-
-
+  const CLASS_UML = {
+    name: 'Person',
+    attributes: ['+name:str','+phoneNumber:str','+emailAddress:str'],
+    methods:['+purcharseParkinPass()'],
+    borderColor:'#DC2DDE',
+    headColor:'#DC2DDE',
+    textColor:'#333333'
+    }
+    
   return (
     <div className="App">
-      <MenuBar config={configMenu}></MenuBar>
+      <DrawClass data={CLASS_UML} />
     </div>
-  )
+  );
 }
 
 export default App;
